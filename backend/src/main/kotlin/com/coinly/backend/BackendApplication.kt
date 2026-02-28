a1f5867bac4b20324f260212c47dd1787e43116a
Thigas017 <tiagofs.morais@gmail.com>
2026-02-28 23:09:46 +0000

    fix: use application.properties to exclude database auto-configuration

--- backend/src/main/kotlin/com/coinly/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/coinly/backend/BackendApplication.kt
@@ -1,11 +1,9 @@
 package com.coinly.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-//TODO Desativa a procura automática por uma base de dados no arranque
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
