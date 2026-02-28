b754d9eec44e4f9f6e531cc18beb97084eb6d551
Thigas017 <tiagofs.morais@gmail.com>
2026-02-28 23:03:36 +0000

    fix: temporary exclude DataSourceAutoConfiguration to pass CI pipeline

--- backend/src/main/kotlin/com/coinly/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/coinly/backend/BackendApplication.kt
@@ -1,11 +1,13 @@
 package com.coinly.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+//TODO Desativa a procura automática por uma base de dados no arranque
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class BackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
+    runApplication<BackendApplication>(*args)
 }
