0afe0ba01d12cedf9d15156ddd095ff827e1ae06
Thigas017 <tiagofs.morais@gmail.com>
2026-02-28 22:56:10 +0000

    fix: resolve kotlin compilation error on disabled test

--- backend/src/test/kotlin/com/coinly/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/coinly/backend/BackendApplicationTests.kt
@@ -1,14 +1,15 @@
 package com.coinly.backend
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Disabled("TODO: Reativar este teste de contextLoads assim que a ligação à base de dados PostgreSQL estiver configurada no application.properties.")
-	@Test
-	fun contextLoads() {
-	}
+    @Disabled("TODO Reativar este teste quando a base de dados estiver configurada")
+    @Test
+    fun contextLoads() {
+    }
 
 }
